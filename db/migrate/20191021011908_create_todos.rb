0725769c5f69ba9de7d4e318be7d5937735c11b2
TairaOuchi <taira.o19@gmail.com>
2019-10-21 13:19:32 +0900

    下準備done

--- db/migrate/20191021011908_create_todos.rb
+++ db/migrate/20191021011908_create_todos.rb
@@ -2,7 +2,7 @@ class CreateTodos < ActiveRecord::Migration[5.2]
   def change
     create_table :todos do |t|
       t.string :title
-
+      t.datetime :deleted_at
       t.timestamps
     end
   end
